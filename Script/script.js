877b408a9d91d0c72b467570690b54193045bb2a
Udegbunam Benjamin (Ben Billion) <107368386+OnyedikaBenjamin@users.noreply.github.com>
2022-10-17 21:00:27 +0100

    Added initial cv website files

---
+++ Script/script.js
@@ -0,0 +1,25 @@
+console.log('Hello World')
+console.log('Benjamin')
+
+name = 'Peter Pan'
+console.log(name)
+food = 20;
+console.log(food);
+fruit = prompt("what is your favouriite fruit");
+
+console.log("fruit");
+
+foodAmount = Number(prompt("What is the amount of food"))
+tipPercentage = Number(prompt("tip % please") / 100);
+tipAmount = foodAmount * tipPercentage;
+console.log(tipAmount);
+alert("tip amount is " + tipAmount + " naira");
+total = foodAmount * tipAmount;
+alert("food total is  " + 'total + naira');
+
+var digerati = "Benjamin"
+console.log(digerati)
+
+name = prompt("What is your name??,  Benjamin")
+
+aleart.log(name)
